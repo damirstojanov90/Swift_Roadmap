343f16a335203177406af715191cb1d7d4d720d4
Damir Stojanov <damir.stojanov@bozic.io>
2026-07-16 14:24:01 +0200

    Phase 4 - Added deliverable.

---
+++ P4/DeliverableApp-P4/DeliverableApp-P4/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DeliverableApp-P4
+//
+//  Created by Damir Stojanov on 27.05.2026..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func calculateAge(age: Int) {
+
+        print("I am doing something.")
+    }
+}
+
