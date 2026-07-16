a22fa43b73f20a4409b56347cf659adac6bd6d0e
Damir Stojanov <damir.stojanov@bozic.io>
2026-07-16 14:30:31 +0200

    Phase 4 - Fix SwiftLint identified issues with formatting.

--- P4/DeliverableApp-P4/DeliverableApp-P4/ViewController.swift
+++ P4/DeliverableApp-P4/DeliverableApp-P4/ViewController.swift
@@ -13,10 +13,4 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    func calculateAge(age: Int) {
-
-        print("I am doing something.")
-    }
 }
-
